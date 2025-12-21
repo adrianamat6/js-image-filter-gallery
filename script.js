0dd88361740a8401e9b7d441bed22e306f72acfc
Adrián Amat Bernabéu <adrianamatbernabeu@gmail.com>
2025-12-21 19:13:47 +0100

    Extendemos funcionalidades de los botones para que se puedan ir clickando entre ellos. También metemos para recuperar el estado inicial si se pulsa de nuevo El botón Todos.

--- script.js
+++ script.js
@@ -1,50 +1,103 @@
 'use-strict'
 
 
-nodoImagenFuego = document.querySelectorAll('.fuego'); 
-nodoImagenTierra = document.querySelectorAll('.tierra'); 
-nodoImagenAgua = document.querySelectorAll('.agua'); 
+const nodoImagenFuego = document.querySelectorAll('.fuego'); 
+const nodoImagenTierra = document.querySelectorAll('.tierra'); 
+const nodoImagenAgua = document.querySelectorAll('.agua'); 
+ 
+const nodoBotonTodos = document.querySelector('#btn-todos');
+const nodoBotonTierra = document.querySelector('#btn-tierra');
+const nodoBotonAgua = document.querySelector('#btn-agua');
+const nodoBotonFuego = document.querySelector('#btn-fuego');
 
-nodoBotonTodos = document.querySelector('#btn-todos');
-nodoBotonTierra = document.querySelector('#btn-tierra');
-nodoBotonAgua = document.querySelector('#btn-agua');
-nodoBotonFuego = document.querySelector('#btn-fuego');
-
-
-
-nodoBotonTodos.addEventListener( 'click' , function(){
-    for (imagenes of nodoImagenFuego){
-        imagenes.style.display = 'none';
-    }
-
-    for (imagenes of nodoImagenTierra){
-        imagenes.style.display = 'none';
-    }
-
-    for (imagenes of nodoImagenAgua){
-        imagenes.style.display = 'none';
-    }
-
-} );
+const nodoContenedorPrincipal = document.querySelector('#botones'); 
 
 nodoBotonTierra.addEventListener( 'click' , function(){
-    for (imagenes of nodoImagenTierra){
+    for (let imagenes of nodoImagenFuego){
         imagenes.style.display = 'none';
     }
 
+    for (let imagenes of nodoImagenTierra){
+        imagenes.style.display = 'block';
+    }
+
+    for (let imagenes of nodoImagenAgua){
+        imagenes.style.display = 'none';
+    }
+
+    
 } );
 
 
 nodoBotonFuego.addEventListener( 'click' , function(){
-    for (imagenes of nodoImagenFuego){
+    for (let imagenes of nodoImagenFuego){
+        imagenes.style.display = 'block';
+    }
+
+    for (let imagenes of nodoImagenTierra){
         imagenes.style.display = 'none';
     }
+
+    for (let imagenes of nodoImagenAgua){
+        imagenes.style.display = 'none';
+    }
+
 
 } );
 
 nodoBotonAgua.addEventListener( 'click' , function(){
-    for (imagenes of nodoImagenAgua){
+    for (let imagenes of nodoImagenFuego){
         imagenes.style.display = 'none';
     }
 
+    for (let imagenes of nodoImagenTierra){
+        imagenes.style.display = 'none';
+    }
+
+    for (let imagenes of nodoImagenAgua){
+        imagenes.style.display = 'block';
+    }
+
+
 } );
+
+let contador = 1; 
+nodoBotonTodos.addEventListener( 'click' , function(){
+    if (contador %2 != 0){
+        ocultar_todos()
+    }else{
+        mostrar_todos()
+    };
+    contador += 1
+} );
+
+
+
+function mostrar_todos(){
+    for (let imagenes of nodoImagenFuego){
+        imagenes.style.display = 'block';
+    }
+
+    for (let imagenes of nodoImagenTierra){
+        imagenes.style.display = 'block';
+    }
+
+    for (let imagenes of nodoImagenAgua){
+        imagenes.style.display = 'block';
+    }
+}
+
+function ocultar_todos(){
+    for (let imagenes of nodoImagenFuego){
+        imagenes.style.display = 'none';
+    }
+
+    for (let imagenes of nodoImagenTierra){
+        imagenes.style.display = 'none';
+    }
+
+    for (let imagenes of nodoImagenAgua){
+        imagenes.style.display = 'none';
+    }
+}
+
